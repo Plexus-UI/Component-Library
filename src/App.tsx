7cb147d92cc63033303d7323f6d92009578005e3
0xtrensetta <ayubfmohammed@gmail.com>
2025-07-05 18:17:22 -0600

    initial

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { CardDemo } from './components/CardDemo'
+import { CalendarDemo } from './components/CalendarDemo'
+import { ButtonDemo } from './components/ButtonDemo.tsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <CardDemo />
+        <CalendarDemo />
+        <ButtonDemo/>
+      </div>
+    </>
+  )
+}
+
+export default App
